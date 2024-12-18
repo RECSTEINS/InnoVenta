82622947b4d5dd69d59708889e34c598a7dc577f
RECSTEINS <raulcedeno_manzanarez@hotmail.com>
2024-12-18 16:05:40 -0500

    Seguridad4

--- cliente/src/Auth/firebaseConfig.js
+++ cliente/src/Auth/firebaseConfig.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyA7xy2xti5Jc2hqQbw7M9FaYQvF5g5_z6c",
-    authDomain: "innoventa-62262.firebaseapp.com",
-    projectId: "innoventa-62262",
-    storageBucket: "innoventa-62262.firebasestorage.app",
-    messagingSenderId: "794774322376",
-    appId: "1:794774322376:web:53af274219b53f862cb9fd",
-    measurementId: "G-N3CW2FZYMF"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
